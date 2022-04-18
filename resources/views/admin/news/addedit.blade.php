72177559b7eb352a914825b0b6c365fcff5f69df
= <=>
2022-04-18 15:01:25 +0700

    sdf

--- resources/views/admin/news/addedit.blade.php
+++ resources/views/admin/news/addedit.blade.php
@@ -20,6 +20,11 @@
     </ul>
 </nav>
 
+<div class="d-sm-flex align-items-center justify-content-between mb-3 flex">
+    <h2 class="h3 mb-0 text-gray-800 line-1 size-1-3-rem">{{ isset($data) ? $data->name : 'Thêm mới' }}</h2>
+    <!-- <a class="add-iteam" href="admin/news/add"><button class="btn-success form-control" type="button"><i class="fa fa-plus" aria-hidden="true"></i> Thêm mới</button></a> -->
+</div>
+
 <div class="row">
     <div class="col-xl-9 col-lg-9">
         <div class="card shadow mb-4">
