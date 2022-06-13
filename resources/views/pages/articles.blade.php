f33f4f1f37ef2575f6959123ab813d79130cf291
= <=>
2022-06-13 16:49:44 +0700

    sdf

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -16,6 +16,22 @@
 <link href="{{asset('')}}frontend/css/widget.css" rel="stylesheet">
 <link href="{{asset('')}}frontend/css/card.css" rel="stylesheet">
 <link href="{{asset('')}}frontend/css/article.css" rel="stylesheet">
+<style type="text/css">
+	
+#toc{background: #f3f3f347; padding: 7px;	font-size: 14px;	text-align: justify;}
+#contents{padding: 0px; }
+#toc ul{padding: 0; margin: 0; }
+#toc ul li{ list-style:none; padding:0px; margin-bottom: 8px; }
+#toc>ul>ul>li{ margin-left: 0px; margin-bottom: 8px; text-align: left; }
+#toc>ul>ul>li>ul{margin-left: 0px}
+#toc>ul>ul>li>ul>li{margin-bottom: 8px;}
+#toc ul li a{ text-decoration:none; }
+#toc ul li a:hover{ color: red }
+#toc ul li .active{color: red}
+#toc ul li{white-space: nowrap; text-overflow: ellipsis; overflow: hidden;}
+.position-sticky h4{font-size: 1.2rem; border-bottom: 1px solid #ddd; }
+
+</style>
 @endsection
 @section('content')
 
@@ -49,7 +65,7 @@
 				</div>
 				<div class="main-content">
 					<div class="row">
-						<div class="col-md-2">
+						<div class="col-md-1">
 							<div class="share-social affix">
 								<span><small>Share</small></span>
 								<ul>
@@ -58,8 +74,8 @@
 								</ul>
 							</div>
 						</div>
-						<div class="col-md-10">
-							<div class="main-article">
+						<div class="col-md-11">
+							<div class="main-article" id="contents">
 								<div class="description">
 									{{$articles->detail}}
 								</div>
@@ -77,8 +93,14 @@
 				</div>
 			</div>
 			<div class="col-lg-3 d-none d-lg-block">
+				<div class="widget affix widget-list mb-3">
+					<div class="position-sticky" style="top: 4.5rem;">
+					<h4><span>Mục lục</span></h4>
+					<div id="toc"  ></div>
+					</div>
+				</div>
 
-				<div class="widget widget-list mb-3">
+				<!-- <div class="widget widget-list mb-3">
 					<h4><span>Tin tức</span></h4>
 					<ul>
 						<li><a href="{{asset('')}}#"><i class="icon-next me-2"></i>Thị trường bất động sản</a></li>
@@ -95,7 +117,7 @@
 						</div>
 					</a>
 					
-				</div>
+				</div> -->
 
 			</div>
 		</div>
@@ -188,5 +210,105 @@
 				}
 			},	
 		});
+</script>
+
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function () {
+  TableOfContents();
+});
+function xu_ly_ky_tu_trang(str) {
+  // Gộp nhiều dấu space thành 1 space
+  str = str.replace(/\s+/g, " ");
+  // loại bỏ toàn bộ dấu space (nếu có) ở 2 đầu của xâu
+  return str.trim();
+}
+function loai_bo_ky_tu_khong_la_chu_so(str) {
+  return str.replace(
+    /[^0-9a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ\s]/gi,
+    ""
+  );
+}
+function xoa_dau(str) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  str = str.replace(/Đ/g, "D");
+  return str;
+}
+function full_xu_ly(str) {
+  str = xu_ly_ky_tu_trang(str);
+  str = loai_bo_ky_tu_khong_la_chu_so(str);
+  str = xoa_dau(str);
+  return str;
+}
+function TableOfContents(container, output) {
+  var toc = "";
+  var level = 0;
+  var container =
+    document.querySelector(container) || document.querySelector("#contents");
+  var output = output || "#toc";
+
+  container.innerHTML = container.innerHTML.replace(
+    /<h([\d])>([^<]+)<\/h([\d])>/gi,
+    function (str, openLevel, titleText, closeLevel) {
+      if (openLevel != closeLevel) {
+        return str;
+      }
+
+      if (openLevel > level) {
+        toc += new Array(openLevel - level + 1).join("<ul>");
+      } else if (openLevel < level) {
+        toc += new Array(level - openLevel + 1).join("</li></ul>");
+      } else {
+        toc += new Array(level + 1).join("</li>");
+      }
+
+      level = parseInt(openLevel);
+
+      var anchor = full_xu_ly(titleText.replace(/ /g, "_"));
+
+      toc += '<li><a class="scroll-to" href="#' + anchor + '">' + titleText + "</a>";
+
+      return (
+        "<h" +
+        openLevel +
+        '  id="' +
+        anchor +
+        '">' +
+        titleText +
+        "</h" +
+        closeLevel +
+        ">"
+      );
+    }
+  );
+
+  if (level) {
+    toc += new Array(level + 1).join("</ul>");
+  }
+  //console.log(toc);
+  document.querySelector(output).innerHTML += toc;
+}
+
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop() + 20;
+    $('h2').each(function(i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('#toc ul ul li a.active').removeClass('active');
+            $('#toc a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
 </script>
 @endsection
