d5e5303de58aeaf990012b5f7b6088a2076e7a9f
= <=>
2022-08-06 09:47:41 +0700

    sdf

--- resources/views/pages/articles_product_bds.blade.php
+++ resources/views/pages/articles_product_bds.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.index')
 
-@section('title'){{ isset($articles->title) ? $articles->title : $articles->name }}@endsection
-@section('description'){{$articles->description}}@endsection
-@section('keywords'){{$articles->keywords}}@endsection
-@section('robots'){{ $articles->robot == 0 ? 'index, follow' : 'noindex, nofollow' }}@endsection
+@section('title'){{ isset($articles->seo->title) ? $articles->seo->title : $articles->name }}@endsection
+@section('description'){{$articles->seo->description}}@endsection
+@section('keywords'){{$articles->seo->keywords}}@endsection
+@section('robots'){{ $articles->seo->robot == 0 ? 'index, follow' : 'noindex, nofollow' }}@endsection
 @section('url'){{asset('').$articles->category->slug.'/'.$articles->slug}}@endsection
 
 @section('css')
